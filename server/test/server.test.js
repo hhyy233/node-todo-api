6bb0a63479f007e1b1a68d3d9d5e0db95dfe2520
hydrogen <wybzdqsmmz@outlook.com>
2018-04-12 15:51:11 -0400

    Add test cases for GET /todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -3,10 +3,13 @@ const request = require('supertest');
 
 const {app} = require('../server');
 const {Todo} = require('../models/todo');
+const {ObjectID} = require('mongodb');
 
 const todos = [{
+	_id: new ObjectID(),
 	text: 'First test todo'
 }, {
+	_id: new ObjectID(),
 	text: 'Second test todo'
 }];
 
@@ -73,5 +76,33 @@ describe('GET /todos', () => {
 			})
 			.end(done); 
 	});
+});
+
+describe('GET /todos/:id', () => {
+	it('should return todo doc', (done) => {
+		request(app)
+			.get(`/todos/${todos[0]._id.toHexString()}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(todos[0].text);
+			})
+			.end(done);
+	});
+	
+	it('should return 404 if todo not found', (done) => {
+		var id = new ObjectID();
+		request(app)
+			.get(`/todos/${id.toHexString()}`)
+			.expect(404)
+			.end(done);
+	});
+	
+	it('should return 404 for non-object ids', (done) => {
+		request(app)
+			.get('/todos/123')
+			.expect(404)
+			.end(done);
+	});
+	
 });
 
