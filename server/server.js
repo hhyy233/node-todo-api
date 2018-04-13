0a5420b3c22b2b01b280633310eb6cdeefcb3661
hydrogen <wybzdqsmmz@outlook.com>
2018-04-12 22:16:49 -0400

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
@@ -72,8 +73,33 @@ app.delete('/todos/:id', (req, res) => {
 	}).catch((e) => {
 		res.status(400).send(e);
 	});
+});
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
 	
-})
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+	
+	if(_.isBoolean(body.completed) && body.completed){
+		body.completedAt = new Date().getTime();
+	}else{
+		body.completed = false;
+		body.completedAt = null;
+	}
+	
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if(!todo){
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e)=>{
+		res.status(400).send();
+	})
+	
+});
 
 app.listen(port, () => {
 	console.log(`Started up at port ${port}...`);
