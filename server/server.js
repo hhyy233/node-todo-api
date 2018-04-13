35156822f4e2d823baadbb493f848c418b4c6da6
hydrogen <wybzdqsmmz@outlook.com>
2018-04-12 21:59:04 -0400

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -64,11 +64,11 @@ app.delete('/todos/:id', (req, res) => {
 	}
 	//remove todo by id
 	
-	Todo.findByIdAndRemove(id).then((doc) => {
-		if(!doc){
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo){
 			return res.status(404).send();
 		}
-		res.status(200).send({doc});
+		res.status(200).send({todo});
 	}).catch((e) => {
 		res.status(400).send(e);
 	});
