0308b842fc4d5536f81e3697ed0cf99b734df262
hydrogen <wybzdqsmmz@outlook.com>
2018-04-14 09:20:47 -0400

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -53,6 +53,17 @@ UserSchema.methods.generateAuthToken = function () {// we have this keyword
 	});
 };
 
+UserSchema.methods.removeToken = function (token) {
+	var user = this;
+	return user.update({
+		$pull: {
+			tokens:{
+				token: token
+			}
+		}
+	});
+};
+
 //model method
 UserSchema.statics.findByToken = function (token) {
 	var User = this;
--- server/server.js
+++ server/server.js
@@ -134,6 +134,13 @@ app.post('/users/login', (req, res) => {
 	
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+	req.user.removeToken(req.token).then(() => {
+		res.status(200).send();
+	}, () => {
+		res.status(400).send();
+	});
+});
 
 app.get('/users/me', authenticate, (req, res) => { 
 	// wrap the authenticate function so we can use the middleware
